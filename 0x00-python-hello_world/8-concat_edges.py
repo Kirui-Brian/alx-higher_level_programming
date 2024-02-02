dbc9bbabb1dadce3f6fe0728fd71779a9fc043a1
Kirui Brian <brinkiru@gmail.com>
2024-02-02 13:05:31 +0300

    correct 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 
-print(str[str.find("object-oriented"):str.find("object-oriented") + 27])
+print(str[str.index("object"):str.index("object") + 27] + " with Python")
