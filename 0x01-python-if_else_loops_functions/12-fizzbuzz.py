d72a181a1616fbfa4d9f8c73b89359ada0df1c23
Kirui Brian <brinkiru@gmail.com>
2024-02-21 12:26:22 +0300

    add 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    """Prints numbers from  1 to 100 with Fizz, Buzz, or FizzBuzz as appropriate."""
+    """Prints numbers from  1 to 100 with Fizz, Buzz, or
+    FizzBuzz as appropriate."""
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
