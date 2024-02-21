65ae6ad81353eac5ff04ab233c7c46c5baae2425
Kirui Brian <brinkiru@gmail.com>
2024-02-21 12:13:40 +0300

    add 11-pow.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,8 @@
+def pow(a, b):
+    """Computes a to the power of b and returns the value"""
+    result = 1
+    for _ in range(abs(b)):
+        result *= a
+    if b < 0:
+        return 1 / result
+    return result
