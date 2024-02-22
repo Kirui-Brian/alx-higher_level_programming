4d7cc9c8c4a199d9a87329c02f0e06af39409caa
Kirui Brian <brinkiru@gmail.com>
2024-02-22 10:28:56 +0300

    add 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    if argc == 0:
+        print("{:d} arguments.".format(argc))
+    elif argc == 1:
+        print("{:d} arguments.".format(argc))
+    else:
+        print("{:d} arguments.".format(argc))
+    for i, arg in enumerate(argv[1:], 1):
+        print("{:d}: {}".format(i, arg))
