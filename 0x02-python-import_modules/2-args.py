5f8f14ad4c985f234dba998c3b62002dd18b60b3
Kirui Brian <brinkiru@gmail.com>
2024-02-22 10:37:56 +0300

    correct 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,7 @@ from sys import argv
 if __name__ == "__main__":
     argc = len(argv) - 1
     if argc == 0:
-        print("{:d} arguments:".format(argc))
+        print("{:d} arguments.".format(argc))
     elif argc == 1:
         print("{:d} argument:".format(argc))
     else:
