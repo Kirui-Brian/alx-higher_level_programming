fe2cbc204f6336c38109174f6d38353182bc025e
Kirui Brian <brinkiru@gmail.com>
2024-05-11 13:45:39 +0300

    add 102.concat.js file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,12 +5,12 @@ const fs = require('fs');
 const [,, fileA, fileB, fileC] = process.argv;
 
 // Read the content of the first file
-const contentA = fs.readFileSync(fileA, { encoding: 'utf8', flag: 'r' });
+const contentA = fs.readFileSync (fileA, { encoding: 'utf8', flag: 'r' });
 
 //Read the content of the second file
-const contentB = fs.readFileSync(fileB, { encoding: 'utf8', flag 'r' });
+const contentB = fs.readFileSync (fileB, { encoding: 'utf8', flag 'r' });
 
 // Concatenate the contents and write to the destination file
-fs.writeFileSync(FileC, contentA + contentB);
+fs.writeFileSync (FileC, contentA + contentB);
 
 console.log(`Contents of ${fileA} and ${fileB} have been concatenated into ${fileC}`);
