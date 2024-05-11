c7179a57a0926e48d21495ceabedf44d9c1841b9
Kirui Brian <brinkiru@gmail.com>
2024-05-11 13:15:43 +0300

    add corrected 8-esrever file

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,7 +2,7 @@
 exports.esrever = function (list) {
   const reversed = [];
   for (let i = list.length - 1; i >= 0; i--) {
-    reversed.push(list[i];
+    reversed.push(list[i]);
   }
   return reversed;
 };
