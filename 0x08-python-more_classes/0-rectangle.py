cc55d9ee2bdc7796bcddef37f3a9d949b0169db7
Kirui Brian <brinkiru@gmail.com>
2024-05-21 16:34:28 +0300

    add  0-rectangle.py file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: 0-rectangle
+
+This module defines an empty Rectangle class.
+"""
+
+
+class Rectangle:
+    """
+    An empty class that defines a rectangle.
+    """
+    pass
