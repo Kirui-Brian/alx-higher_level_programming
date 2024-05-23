da0da5afe7b010ec89aadc1e00695967f0842b33
Kirui Brian <brinkiru@gmail.com>
2024-05-23 16:11:57 +0300

    add 6-completed_tasks.js file

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('error:', error);
+  } else {
+    const tasks = JSON.parse(body);
+    const completedTasks = {};
+
+    tasks.forEach(task => {
+      if (task.completed) {
+        if (completedTasks[task.userId]) {
+          completedTasks[task.userId]++;
+        } else {
+          completedTasks[task.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasks);
+  }
+});
