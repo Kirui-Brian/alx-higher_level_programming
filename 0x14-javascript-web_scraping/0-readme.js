782ebae0ba93fb567034c65fecd1050e0f1d0ddf
Kirui Brian <brinkiru@gmail.com>
2024-05-23 16:04:26 +0300

    add 0-readme.js file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,11 +2,10 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-
 fs.readFile(filePath, 'utf8', (err, data) => {
-	if (err) {
-		console.error(err);
-	} else {
-		console.log(data);
-	}
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
 });
