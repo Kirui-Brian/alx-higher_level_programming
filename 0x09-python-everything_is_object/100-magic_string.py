76eda3daa4ed4eeea6573d5de749c1d8a1e8bad2
Kirui Brian <brinkiru@gmail.com>
2024-05-24 18:57:53 +0300

    add corrected 100-magic_string.py file

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(iterations=[0]):
+def magic_string(iterations=[-1]):
     iterations[0] += 1
-    return "BestSchool, " * iterations[0]
+    return "BestSchool" + ", BestSchool" * iterations[0]
